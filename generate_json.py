1989f51a4f01828271248e8ed1aa361e750ff755
Computers Robbers <computerandrobbers@gmail.com>
2024-03-17 14:36:08 -0400

    Update README.md

--- generate_json.py
+++ generate_json.py
@@ -83,7 +83,7 @@ if __name__ == "__main__":
             raw_md = f.read()
         html = mistletoe.markdown(raw_md)
         soup = BeautifulSoup(html, "html.parser")
-        table = soup.find_all("table")[1]
+        table = soup.find_all("table")[0]
         md_df = pd.read_html(StringIO(str(table)), keep_default_na=False)[0]
         md_df["App Name"] = md_df["App Name"].str.replace(" ", "").str.lower()
 
